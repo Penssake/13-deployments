41271986cc34e25186323ca1a3f995e4a4a617a3
Christina Thomas <Penssake@gmail.com>
2017-04-28 11:08:49 -0600

    lost about me section

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -10,15 +10,15 @@
     //       Don't forget to remove the headers from our request - we're no longer using a token on the
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
-    $.get('/github/users/penssake/repos', function(data){
-      console.log(data);
-    })
-
-    // $.ajax({
-    //   url: `https://api.github.com/user/repos`,
-    //   type: 'GET',
-    //   headers: {'Authorization': `token ${githubToken}`}
+    // $.get('/github/users/penssake/repos', function(data){
+    //   console.log(data);
     // })
+
+    $.ajax({
+      url: `https://api.github.com/penssake/repos`,
+      type: 'GET',
+      headers: {'Authorization': `token ${githubToken}`}
+    })
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
